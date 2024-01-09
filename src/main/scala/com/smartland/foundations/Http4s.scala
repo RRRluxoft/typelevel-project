2a830d9f125502acbb0df709a9df5f54af5a6a17
Roman Lytvyshko <roman.lytvyshko@ocado.com>
2024-01-10 00:40:39 +0100

    code formatting

--- src/main/scala/com/smartland/foundations/Http4s.scala
+++ src/main/scala/com/smartland/foundations/Http4s.scala
@@ -7,6 +7,7 @@ import io.circe.syntax.*
 import org.http4s.circe.*
 import cats.effect.IOApp
 import cats.effect.IO
+
 import java.util.UUID
 import org.http4s.QueryParamDecoder
 import org.http4s.ParseFailure
@@ -26,6 +27,7 @@ import org.typelevel.ci.CIString
 import org.http4s.server.Router
 import Http4s.CourseREpository.allRoutes
 import Http4s.CourseREpository.routerWithPathPrefixes
+import com.comcast.ip4s.Port
 
 object Http4s extends IOApp.Simple {
 
@@ -33,8 +35,7 @@ object Http4s extends IOApp.Simple {
   type Student = String
   case class Instructor(firstName: String, lastName: String)
   case class Course(id: String, title: String, year: Int, students: List[Student], instructorNme: String)
-     
-     
+
   object CourseREpository {
     val catsEffectCourse = Course(
       id = "a456df8e-9d20-4cfa-8b47-d73eb46f0ed9",
@@ -48,45 +49,42 @@ object Http4s extends IOApp.Simple {
 
     // API
     def findCourseById(courseId: UUID): Option[Course] = courses.get(courseId.toString)
-    def findCourseByInstructor(instructorNme: String): List[Course] = 
+    def findCourseByInstructor(instructorNme: String): List[Course] =
       courses.values.filter(_.instructorNme == instructorNme).toList
- 
+
     // Endpoints
     // GET 'localhost:8080/courses?instructor=Marcin%20Odersky&year=2024'
     // GET 'localhost:8080/courses/a456df8e-9d20-4cfa-8b47-d73eb46f0ed9/students'
     object InstructorQueryParameterMatcher extends QueryParamDecoderMatcher[String]("instructor")
-    object YearQueryParameterMatcher extends OptionalValidatingQueryParamDecoderMatcher[Int]("year")
+    object YearQueryParameterMatcher       extends OptionalValidatingQueryParamDecoderMatcher[Int]("year")
 
     def courseRoutes[F[_]: Monad]: HttpRoutes[F] = {
       val dsl = Http4sDsl[F]
-      import dsl.* 
+      import dsl.*
 
       HttpRoutes.of[F] {
-        case GET -> Root / "courses" :? InstructorQueryParameterMatcher(instructor) +& YearQueryParameterMatcher(maybeYear) => 
+        case GET -> Root / "courses" :? InstructorQueryParameterMatcher(instructor) +& YearQueryParameterMatcher(maybeYear) =>
           val courses = CourseREpository.findCourseByInstructor(instructor)
           maybeYear.fold(Ok(courses.asJson)) { y =>
-            y.fold(_ => BadRequest("Invalid year"),
-            year => Ok(courses.filter(_.year == year).asJson)
-            )
+            y.fold(_ => BadRequest("Invalid year"), year => Ok(courses.filter(_.year == year).asJson))
           }
 
         case GET -> Root / "courses" / UUIDVar(courseId) / "students" =>
           CourseREpository.findCourseById(courseId)
-          .fold(NotFound(s"No course with $courseId was found")) { c =>
-            Ok(c.students.asJson, Header.Raw(CIString("My-custom-header"), "smartland"))
-          }
-          // .map(_.students) match
-          //   case None => NotFound(s"No course with $courseId")
-          //   case Some(students) => Ok(students.asJson)
-          
+            .fold(NotFound(s"No course with $courseId was found")) { c =>
+              Ok(c.students.asJson, Header.Raw(CIString("My-custom-header"), "smartland"))
+            }
+        // .map(_.students) match
+        //   case None => NotFound(s"No course with $courseId")
+        //   case Some(students) => Ok(students.asJson)
 
-        case GET -> Root => Ok("test ok!") 
+        case GET -> Root => Ok("test ok!")
       }
     }
 
     def healthEndpoint[F[_]: Monad]: HttpRoutes[F] = {
       val dsl = Http4sDsl[F]
-      import dsl.* 
+      import dsl.*
 
       HttpRoutes.of[F] {
         case GET -> Root / "health" => Ok("All going well!")
@@ -95,23 +93,21 @@ object Http4s extends IOApp.Simple {
 
     def allRoutes[F[_]: Monad]: HttpRoutes[F] = courseRoutes[F] <+> healthEndpoint[F]
 
-
     // GET 'localhost:8080/api/courses?instructor=Marcin%20Odersky&year=2024'
     // GET 'localhost:8080/api/courses/a456df8e-9d20-4cfa-8b47-d73eb46f0ed9/students'
     // GET 'localhost:8080/private/health'
     def routerWithPathPrefixes = Router(
-      "/api" -> courseRoutes[IO],
+      "/api"     -> courseRoutes[IO],
       "/private" -> healthEndpoint[IO]
     ).orNotFound
   }
 
-
-  override def run: IO[Unit] = 
+  override def run: IO[Unit] =
     // IO.println(UUID.randomUUID().toString())
     EmberServerBuilder.default[IO]
-    .withHttpApp(routerWithPathPrefixes)
-    .build
-    .use(_ => IO.println("Server ready!") *> IO.never)
-
+      .withPort(Port.fromInt(18080).get)
+      .withHttpApp(routerWithPathPrefixes)
+      .build
+      .use(_ => IO.println("Server ready!") *> IO.never)
 
 }
